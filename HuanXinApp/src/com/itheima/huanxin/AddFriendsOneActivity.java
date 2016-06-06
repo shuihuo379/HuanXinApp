fbff32e31066fefd584949aca7044e8f0a636e98
shuihuo379 <1920229414@qq.com>
2016-06-06 11:11:46 +0800

    添加好友剩余界面

--- HuanXinApp/src/com/itheima/huanxin/AddFriendsOneActivity.java
+++ HuanXinApp/src/com/itheima/huanxin/AddFriendsOneActivity.java
@@ -1,6 +1,10 @@
 package com.itheima.huanxin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 /**
  * 添加好友界面(第一步)
@@ -11,5 +15,18 @@ public class AddFriendsOneActivity extends BaseActivity{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_addfriends_one);
+		
+		TextView tv_search=(TextView) this.findViewById(R.id.tv_search);
+        tv_search.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v) {
+            	startActivity(new Intent(AddFriendsOneActivity.this,AddFriendsTwoActivity.class));                
+            }
+        });
 	}
+	
+	public void back(View view){
+        finish();
+    }
 }
